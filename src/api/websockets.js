410f0ad69bc0a37f2b16dff5c0c760523387414f
Mahmoud Ikhlaf <513095@student.fontys.nl>
2024-01-04 18:14:52 +0100

    Fixed little mistake

--- src/api/websockets.js
+++ src/api/websockets.js
@@ -2,11 +2,11 @@ const getWsUrl = () => {
     let url;
     switch(import.meta.env.MODE) {
       case 'production':
-        url = import.meta.env.VITE_DEV_RUNNER_WS_URL;
+        url = import.meta.env.VITE_PROD_RUNNER_WS_URL;
         break;
       case 'development':
       default:
-        url = import.meta.env.VITE_PROD_RUNNER_WS_URL;
+        url = import.meta.env.VITE_DEV_RUNNER_WS_URL;
     }
     return url;
 }
